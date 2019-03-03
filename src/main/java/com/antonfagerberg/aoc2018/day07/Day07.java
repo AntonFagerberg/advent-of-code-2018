2d7e8af5ec6399899b5bbb08b22a5ce334d1e5c0
Anton Fagerberg <anton@antonfagerberg.com>
2019-03-03 17:51:24 +0100

    Solved day 7 part 2

--- src/main/java/com/antonfagerberg/aoc2018/day07/Day07.java
+++ src/main/java/com/antonfagerberg/aoc2018/day07/Day07.java
@@ -3,6 +3,8 @@ package com.antonfagerberg.aoc2018.day07;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
@@ -50,6 +52,80 @@ class Day07 {
             } else {
                 result += available.pollFirst();
             }
+        }
+    }
+
+
+    public static int part2(String[] input, int workers, int duration) {
+        var edges = Arrays.stream(input)
+                .map(sentence -> {
+                    var words = sentence.split(" ");
+                    var from = words[1];
+                    var to = words[7];
+                    return Pair.of(from.charAt(0), to.charAt(0));
+                })
+                .collect(Collectors.toList());
+
+
+        var available = edges.stream()
+                .filter(edge -> edges.stream().noneMatch(e -> e.getRight().equals(edge.getLeft())))
+                .map(Pair::getLeft)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toCollection(TreeSet::new));
+
+        var result = "";
+        var elapsedTime = 0;
+
+        var workTimers = new int[workers];
+        var workerAllocation = new HashMap<Integer, Character>();
+        var used = new HashSet<Character>();
+
+        while (true) {
+            String finalResult = result;
+
+            var newAvailable = edges.stream()
+                    .filter(edge -> !used.contains(edge.getRight()))
+                    .map(Pair::getRight)
+                    .distinct()
+                    .filter(node ->
+                            edges.stream()
+                                    .filter(edge -> edge.getRight().equals(node))
+                                    .allMatch(edge -> finalResult.indexOf(edge.getLeft()) != -1)
+                    )
+                    .sorted()
+                    .collect(Collectors.toCollection(TreeSet::new));
+
+            available.addAll(newAvailable);
+
+            if (available.isEmpty() && workerAllocation.isEmpty()) {
+                return elapsedTime;
+            } else {
+                for (int i = 0; i < workTimers.length; i++) {
+                    if (workTimers[i] > 0) {
+                        workTimers[i] -= 1;
+                    }
+
+                    if (workTimers[i] == 0) {
+                        var done = workerAllocation.remove(i);
+
+                        if (done != null) {
+                            result += done;
+                        }
+
+                        var allocation = available.pollFirst();
+
+                        if (allocation != null) {
+                            used.add(allocation);
+                            workerAllocation.put(i, allocation);
+                            workTimers[i] = allocation + duration - 'A';
+                        }
+                    }
+                }
+
+                elapsedTime += 1;
+            }
+
         }
     }
 
--- src/test/java/com/antonfagerberg/aoc2018/day07/Day07Test.java
+++ src/test/java/com/antonfagerberg/aoc2018/day07/Day07Test.java
@@ -26,10 +26,29 @@ public class Day07Test {
 
     @Test
     public void testPart1() throws IOException, URISyntaxException {
-
-        // JMZELQVYXTIGPHFNSOADKWBRUC NOPE?
         var input = Input.get("day07/input").toArray(String[]::new);
-
         Assert.assertEquals("JMQZELVYXTIGPHFNSOADKWBRUC", Day07.part1(input));
     }
+
+    @Test
+    public void testPart2Example1() {
+        var input = new String[]{
+                "Step C must be finished before step A can begin.",
+                "Step C must be finished before step F can begin.",
+                "Step A must be finished before step B can begin.",
+                "Step A must be finished before step D can begin.",
+                "Step B must be finished before step E can begin.",
+                "Step D must be finished before step E can begin.",
+                "Step F must be finished before step E can begin."
+        };
+
+        Assert.assertEquals(15, Day07.part2(input, 2, 0));
+    }
+
+    @Test
+    public void testPart2() throws IOException, URISyntaxException {
+        var input = Input.get("day07/input").toArray(String[]::new);
+        Assert.assertEquals(1133, Day07.part2(input, 5, 60));
+    }
+
 }
